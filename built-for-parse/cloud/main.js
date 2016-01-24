ef3db8fedd2ee42eeb0c63f0651ad276a88bbaaf
Owen Campbell-Moore <owencmoore@gmail.com>
2016-01-24 15:04:57 -0800

    Restructured project around parse

---
+++ built-for-parse/cloud/main.js
@@ -0,0 +1,10 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function (request, response) {
+  response.success("Hello world!");
+});
+
+},{}]},{},[1]);
